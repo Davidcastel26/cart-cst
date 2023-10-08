e33a80689284748cc6288ff5bc7896b8cfeae619
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-08 17:33:31 -0600

    creating totals

--- src/utils.js
+++ src/utils.js
@@ -4,5 +4,11 @@ export const getTotals = (cart) => {
     let totalAmount = 0;
     let totalCost = 0;
 
+    for( let {amount, price} of cart.values()){
+        // console.log(item)
+        totalAmount += amount;
+        totalCost += amount * price;
+    }
+
     return {totalAmount, totalCost};
 }
